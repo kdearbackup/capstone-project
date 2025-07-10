781fc572033d955da4f0369c8adbcc90d0b03799
Sakhawat Hossain <s19hossain98@gmail.com>
2025-07-10 10:14:11 -0400

    Migrate to TypeScript: Replace app.js/server.js with app.ts/server.ts

---
+++ server/eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import json from '@eslint/json';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['src/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    ignores: ['.node_modules/*', 'dist', '*.json'],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'prefer-const': 'error',
+    },
+  },
+  eslintPluginPrettierRecommended,
+]);
